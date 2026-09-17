5f95e76b16a73a50635275c878293a544eac5c56
Ian Buchanan <ibuchanan@atlassian.com>
2026-09-17 17:39:34 -0500

    feat(forge): NONE Add remote form UI

--- test/manifest.test.sh
+++ test/manifest.test.sh
@@ -7,3 +7,6 @@ test "$(yq -r '.modules."jira:issueContext"[0].resolver.endpoint' "$root/apps/fo
 test "$(yq -r '.modules.endpoint[0] | has("route")' "$root/apps/forge/manifest.yml")" = false
 test "$(yq -r '.permissions.scopes | length' "$root/apps/forge/manifest.yml")" = 0
 test -f "$root/apps/forge/resources/index.html"
+grep -q 'src="index.js"' "$root/apps/forge/resources/index.html"
+test -f "$root/apps/forge/src/index.ts"
+grep -q 'invokeRemote' "$root/apps/forge/src/index.ts"
